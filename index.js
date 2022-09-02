c757c52eb66e6efc05cf4e017a13678ff036352a
teku <teku145259525@gmail.com>
2022-09-03 01:43:08 +0900

    add new file

---
+++ index.js
@@ -0,0 +1,31 @@
+$(function() {
+    $("#menu-btn").on("click", function(){
+        if($("#menu-btn").hasClass("active")){
+            $(this).removeClass("active");
+            $("menu").slideUp();
+        }else{
+            $(this).toggleClass("active");
+            $("menu").slideDown();
+        }
+    });
+
+    $("#2022").on("click", function(){
+        if($("#2022").hasClass("active")){
+            $(this).removeClass("active");
+            $('.subLog2022').slideUp();
+        }else{
+            $(this).toggleClass("active");
+            $('.subLog2022').slideDown();
+        }
+    });
+
+    $("#2021").on("click", function(){
+        if($("#2021").hasClass("active")){
+            $(this).removeClass("active");
+            $('.subLog2021').slideUp();
+        }else{
+            $(this).toggleClass("active");
+            $('.subLog2021').slideDown();
+        }
+    });
+})
